3cbc42b01528a598f407a9aaa595615654da8087
Kenzie Togami <ket1999@gmail.com>
2013-12-21 10:02:30 -0800

    Make smoke more random.

--- 172Bukkit/src/k/tp/executors/BlackSmoke.java
+++ 172Bukkit/src/k/tp/executors/BlackSmoke.java
@@ -9,24 +9,36 @@ public class BlackSmoke extends BaseCE {
 
 	@Override
 	void preTeleport(Player sp, Location strike1, Location strike2, boolean p2p) {
-		sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
-		sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
-		sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
-		sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
-		sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
+		Location[] locs = randomize(strike1, 10);
+		for (Location l : locs) {
+			sp.getWorld().playEffect(l, Effect.SMOKE, BlockFace.UP);
+		}
 	}
 
 	@Override
 	void postTeleport(Player sp, String teleString, Location strike1,
 			Location strike2, boolean p2p, boolean didTele) {
 		if (didTele) {
-			sp.getWorld().playEffect(strike2, Effect.SMOKE, BlockFace.UP);
-			sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
-			sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
-			sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
-			sp.getWorld().playEffect(strike1, Effect.SMOKE, BlockFace.UP);
+			Location[] locs = randomize(strike2, 10);
+			for (Location l : locs) {
+				sp.getWorld().playEffect(l, Effect.SMOKE, BlockFace.UP);
+			}
 			sp.sendMessage("Teleported you to " + teleString + " with smoke!");
 		}
 	}
 
+	private Location[] randomize(Location l, int times) {
+		double[] orig = new double[] { l.getX(), l.getY(), l.getZ() };
+		double[][] random = new double[times][3];
+		Location[] out = new Location[times];
+		for (int i = 0; i < random.length; i++) {
+			for (int j = 0; j < random[0].length; j++) {
+				random[i][j] = orig[j]
+						+ (Math.random() > 0.5 ? -Math.random() : Math.random());
+			}
+			out[i] = new Location(l.getWorld(), random[i][0], random[i][1],
+					random[i][2], l.getYaw(), l.getPitch());
+		}
+		return out;
+	}
 }
